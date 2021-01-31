af3f39e6ff0f12908edfc03b2caf5107681873f6
goncharoi <s--e--t@yandex.ru>
2021-01-31 13:36:03 +0500

    lesson02 done

--- lesson02/src/lesson02/Main.java
+++ lesson02/src/lesson02/Main.java
@@ -8,37 +8,39 @@ public class Main {
         System.out.println("Тест 1: ругаемся на длину массива");
         System.out.println("*********************************");
         String[][] ltTest01 = {{"0","1","1","5"},{"0","1","1"},{"0","1","1"},{"0","1","1"}};
-        checkArrayWithoutEx(ltTest01);
+        printSumArray(ltTest01);
 
         System.out.println("*********************************");
         System.out.println("Тест 2: ругаемся на неправильный символ");
         System.out.println("*********************************");
         String[][] ltTest02 = {{"0","1","1","5"},{"0","1","3","1"},{"4","0","1","1"},{"Ошибочка","5","6","9"}};
-        checkArrayWithoutEx(ltTest02);
+        printSumArray(ltTest02);
 
         System.out.println("*********************************");
         System.out.println("Тест 3: ни на что не ругаемся");
         System.out.println("*********************************");
         String[][] ltTest03 = {{"0","1","1","5"},{"0","1","3","1"},{"4","0","1","1"},{"0","5","6","9"}};
-        checkArrayWithoutEx(ltTest03);
+        printSumArray(ltTest03);
     }
-    public static void checkArray(String[][] itArray) throws MyArrayDataException, MyArraySizeException {
+    public static int getSumArray(String[][] itArray) throws MyArrayDataException, MyArraySizeException {
+        int rvSumm = 0;
         if (itArray.length != SIZE) throw new MyArraySizeException();
         for (int i = 0; i < SIZE; i++) {
             if (itArray[i].length != SIZE) throw new MyArraySizeException();
             for (int j = 0; j < SIZE; j++) {
                 try {
-                    int lvTest = Integer.parseInt(itArray[i][j]);
+                    rvSumm += Integer.parseInt(itArray[i][j]);
                 } catch (NumberFormatException loEx){
                     throw new MyArrayDataException(i,j);
                 }
             }
         }
+        return rvSumm;
     }
     //обертка над checkArray
-    public static void checkArrayWithoutEx(String[][] itArray){
+    public static void printSumArray(String[][] itArray){
         try{
-            checkArray(itArray);
+            System.out.println("Сумма элементов массива = " + getSumArray(itArray));
         } catch (MyArraySizeException loMASEx){
             loMASEx.printStackTrace(System.out);
         } catch (MyArrayDataException loMADEx) {
